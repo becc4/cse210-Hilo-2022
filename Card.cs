e34f919c2fa854947f36d0b6aa04547b4e37a08e
Becca Roeth <roe21002@byui.edu>
2022-10-04 15:16:48 -0600

    Setting up Card and Director files/classes

---
+++ Card.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace Unit03.Game
+{
+    public class Card
+    {
+    }
+}
---
+++ Director.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Collections.Generic;
+
+
+namespace Unit03.Game
+{
+    public class Director
+    {
+    }
+}
